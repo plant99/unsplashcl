d5ec596482a0231a6bab0f618289efb7bf3afa91
Tanish Grover <tanigrover@gmail.com>
2019-12-24 21:41:07 +0530

    add support for KDE, Unity and Cinnamon desktop env

--- desktopi/__main__.py
+++ desktopi/__main__.py
@@ -13,6 +13,28 @@ def isX64():
     import struct
     return struct.calcsize('P') * 8 == 65
 
+
+def get_linux_desktop_env():
+    desktop_session = os.environ.get("DESKTOP_SESSION")
+    
+    if desktop_session in ["gnome", "kde", "unity", "cinnamon"]:
+        return desktop_session
+    
+    elif desktop_session.startswith("ubuntu"):
+        return "unity"
+
+    elif desktop_session.startswith("kubuntu"):
+        return "kde"
+    
+    elif os.environ.get("KDE_FULL_SESSION") == 'true':
+        return "kde"
+    
+    elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
+        return "gnome"
+    
+    return None
+    
+
 def change_background(image_path):
     os_name = platform.system()
     try: 
@@ -21,7 +43,37 @@ def change_background(image_path):
             subprocess.check_call(["killall Dock"], shell=True)
 
         elif os_name == "Linux":
-            os.system("gsettings set org.gnome.desktop.background picture-uri file://" + image_path)
+            desktop_env = get_linux_desktop_env()
+            uri = f'"file://{image_path}"'
+
+            if desktop_env in ["gnome", "unity"]:
+                args = ["gsettings", "set", "org.gnome.desktop.background", "picture-uri", uri]
+                subprocess.check_call(args)
+            
+            elif desktop_env == "cinnamon":    
+                args = ["gsettings", "set", "org.cinnamon.desktop.background", "picture-uri", uri]
+                subprocess.check_call(args)
+
+            elif desktop_env == "kde":
+                script = """
+                string: 
+                var allDesktops = desktops();
+                print (allDesktops);
+                for (i=0;i<allDesktops.length;i++) {
+                    d = allDesktops[i];
+                    d.wallpaperPlugin = "org.kde.image";
+                    d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+                    d.writeConfig("Image", "file://%s")
+                } 
+                """ % uri
+                
+                # If this doesn't work try the other args
+                args = ["dbus-send", "--session", "--dest=org.kde.plasmashell", "--type=method_call", "/PlasmaShell", "org.kde.PlasmaShell.evaluateScript", "%s" % script]
+                # args = ["qdbus", "org.kde.plasmashell", "/PlasmaShell", "evaluateScript", "%s" % script]
+                subprocess.check_call(args)
+                
+            else:
+                raise Exception("Linux Desktop Environment Not Supported")
 
         elif os_name == "Windows":
             import ctypes
