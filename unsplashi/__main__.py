ee9f928fac07f103161febdf3593253f6b2f00c4
chiragbablani0 <42476557+chiragbablani0@users.noreply.github.com>
2020-01-08 23:26:51 +0530

    Update __main__.py

--- unsplashi/__main__.py
+++ unsplashi/__main__.py
@@ -1,11 +1,11 @@
 import requests
+import config
 
 def getImage(url):
-	client_id = "a235801b84ae3057163a384548acbb74f82dfc72ee2e8e9580637271ea1a7858"
 	list1 = url.split('/')
 	id = list1[-1]
 	endpoint = "https://api.unsplash.com/photos/"
-	actual_url = endpoint+id+"?"+"client_id="+client_id
+	actual_url = endpoint+id+"?"+"client_id="+config.client_id
 	
 	r_data = requests.get(actual_url)
 	r_dict=r_data.json()
